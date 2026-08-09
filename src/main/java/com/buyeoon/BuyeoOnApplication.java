4ea59ec0b4a4521379582e8444b3ca10307e4230
jdk829355 <jdk829355@gmail.com>
2026-08-09 21:56:59 +0900

    chore: initialize Spring Boot project scaffold with Java 21

---
+++ src/main/java/com/buyeoon/BuyeoOnApplication.java
@@ -0,0 +1,13 @@
+package com.buyeoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyeoOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyeoOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buyeoon/BuyeoOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyeoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyeoOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
