48678befe513037c526eada693fd68dbe856eb56
JUNG DAEGYUN <64572472+jdk829355@users.noreply.github.com>
2026-08-19 18:25:21 +0900

    fix: ApplicationContext 시작 충돌 제거 (#121) (#129)

--- src/main/java/com/buyeoon/point/repository/PointTransactionRepository.java
+++ src/main/java/com/buyeoon/point/repository/PointTransactionRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-/** 회원의 포인트 잔액·누적 적립 합계와 내역 목록을 조회하는 조회 전용 repository다. */
+/** 회원의 포인트 내역을 저장하고 잔액·누적 적립 합계와 내역 목록을 조회하는 repository다. */
 public interface PointTransactionRepository extends JpaRepository<PointTransactionEntity, UUID> {
 
 	/** 회원의 포인트 잔액(전체 내역 amount 합계)을 계산한다. 내역이 없으면 0을 반환한다. */
